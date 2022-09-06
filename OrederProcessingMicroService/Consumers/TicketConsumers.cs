b2ebd51076ad951cc3b78cefcb139662a2c922b2
Vishwas <baghel.vishwas0@gmail.com>
2022-09-06 15:18:48 +0530

    [Vishwas]added: RabbitMQ Project Created

---
+++ OrederProcessingMicroService/Consumers/TicketConsumers.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Shared.Models.Models;
+using System.Threading.Tasks;
+
+namespace OrederProcessingMicroService.Consumers
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
---
+++ Shared.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Shared.Model
+{
+    public class Class1
+    {
+    }
+}
